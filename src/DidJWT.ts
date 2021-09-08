f0be0fbccff1fecf26d81eb7c2822633f5c48d9a
nklomp <nklomp@sphereon.com>
2021-09-09 00:47:10 +0200

    Rename Did JWT methods

--- src/DidJWT.ts
+++ src/DidJWT.ts
@@ -3,7 +3,7 @@ import {JWTHeader, JWTOptions, JWTPayload, VerifiedJWT, VerifyOptions} from "./J
 
 
 
-export async function verifyJWT(
+export async function verify(
     jwt: string,
     options: VerifyOptions = {
         audience: undefined,
@@ -18,8 +18,24 @@ export async function verifyJWT(
 
 
 
-
-export async function createJWT(
+/**
+ *  Creates a signed JWT given an address which becomes the issuer, a signer function, and a payload for which the signature is over.
+ *
+ *  @example
+ *  const signer = ES256KSigner(process.env.PRIVATE_KEY)
+ *  createJWT({address: '5A8bRWU3F7j3REx3vkJ...', signer}, {key1: 'value', key2: ..., ... }).then(jwt => {
+ *      ...
+ *  })
+ *
+ *  @param    {Object}            payload               payload object
+ *  @param    {Object}            [options]             an unsigned credential object
+ *  @param    {String}            options.issuer        The DID of the issuer (signer) of JWT
+ *  @param    {Signer}            options.signer        a `Signer` function, Please see `ES256KSigner` or `EdDSASigner`
+ *  @param    {boolean}           options.canonicalize  optional flag to canonicalize header and payload before signing
+ *  @param    {Object}            header                optional object to specify or customize the JWT header
+ *  @return   {Promise<Object, Error>}                  a promise which resolves with a signed JSON Web Token or rejects with an error
+ */
+export async function create(
     payload: Partial<JWTPayload>,
     { issuer, signer, expiresIn, canonicalize }: JWTOptions,
     header: Partial<JWTHeader> = {}
