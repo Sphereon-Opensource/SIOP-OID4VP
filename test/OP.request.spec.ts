9a7507f13300b93fa4befc26cb6cbba7b3efab10
nklomp <nklomp@sphereon.com>
2021-09-23 22:36:17 +0200

    Fix json to URI encoding

--- test/OP.request.spec.ts
+++ test/OP.request.spec.ts
@@ -14,6 +14,7 @@ import {mockedGetEnterpriseAuthToken} from "./TestUtils";
 
 const EXAMPLE_REDIRECT_URL = "https://acme.com/hello";
 const EXAMPLE_REFERENCE_URL = "https://rp.acme.com/siop/jwts";
+
 const HEX_KEY = "f857544a9d1097e242ff0b287a7e6e90f19cf973efe2317f2a4678739664420f";
 const DID = "did:ethr:0x0106a2e985b1E1De9B5ddb4aF6dC9e928F4e99D0";
 const KID = "did:ethr:0x0106a2e985b1E1De9B5ddb4aF6dC9e928F4e99D0#controller";
