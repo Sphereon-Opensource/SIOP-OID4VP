92b40b363c1160d8bc6948e2ec2ba764dcc83729
nklomp <nklomp@sphereon.com>
2021-09-29 17:40:23 +0200

    Exclude did-jwt fork from coverage

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
     coverageDirectory: "./coverage/",
     collectCoverageFrom: [
-        "!did-jwt-fork/**",
+        "!src/did-jwt-fork/**",
         "!src/schemas/**",
         "!src/**/*.d.ts",
         "!**/node_modules/**",
