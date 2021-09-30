9ca789e51566b8af9d1dda9216fb5bb0af6ec2c1
sksadjad <86593551+sksadjad@users.noreply.github.com>
2021-09-30 13:44:06 +0200

    ONTO-58: import changed to right place

--- test/PEManager.spec.ts
+++ test/PEManager.spec.ts
@@ -1,9 +1,9 @@
 import { Presentation, VP } from '@sphereon/pe-js';
 import { PresentationDefinition } from '@sphereon/pe-models';
 
-import { SIOPErrors } from '../dist/main/types';
 import { PEManager, SIOP } from '../src/main';
 import { State } from '../src/main/functions';
+import { SIOPErrors } from '../src/main/types';
 import {
   AuthenticationRequestPayload,
   CredentialFormat,
