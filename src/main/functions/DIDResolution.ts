1cab5b6a06d1e4fd05ba292d3a7d008453805264
sksadjad <86593551+sksadjad@users.noreply.github.com>
2022-09-22 09:56:31 +0200

    chore: added doc for getResolverUnion method

--- src/main/functions/DIDResolution.ts
+++ src/main/functions/DIDResolution.ts
@@ -31,6 +31,17 @@ export function getResolver(opts: ResolveOpts): Resolvable {
   }
 }
 
+/**
+ * This method returns a resolver object in OP/RP
+ * If the user of this library, configures OP/RP to have a customResolver, we will use that
+ * If the user of this library configures OP/RP to use a custom resolver for any specific did method, we will use that
+ * and in the end for the rest of the did methods, configured either with calling `addDidMethod` upon building OP/RP
+ * (without any resolver configuration) or declaring in the subject_syntax_types_supported of the registration object
+ * we will use universal resolver from Sphereon's DID Universal Resolver library
+ * @param customResolver
+ * @param subjectSyntaxTypesSupported
+ * @param resolverMap
+ */
 export function getResolverUnion(
   customResolver: Resolvable,
   subjectSyntaxTypesSupported: string[] | string,
