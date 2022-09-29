252b54f24a96b12cefa90ee793f298d569dc34ac
hrehman-sphereon <hrehman@sphereon.com>
2022-09-29 13:50:31 +0200

    [VDX-148] improve documentation

--- src/main/LanguageTagUtils.ts
+++ src/main/LanguageTagUtils.ts
@@ -5,19 +5,38 @@ import { SIOPErrors } from './types';
 export default class LanguageTagUtils {
   private static readonly LANGUAGE_TAG_SEPARATOR = '#';
 
+  /**
+   * It will give back a fields which are language tag enabled. i.e. all fields with the fields names containing
+   * language tags e.g. fieldName#nl-NL
+   *
+   * @param source is the object from which the language enabled fields and their values will be extracted.
+   */
   static getAllLanguageTaggedProperties(source: unknown): Map<string, string> {
     return this.getLanguageTaggedPropertiesMapped(source, undefined);
   }
 
-  static getLanguageTaggedProperties(source: unknown, languageTagEnabledFieldsNames: Array<string>): Map<string, string> {
+  /**
+   * It will give back a fields which are language tag enabled and are listed in the required fields.
+   *
+   * @param source is the object from which the language enabled fields and their values will be extracted.
+   * @param requiredFieldNames the fields which are supposed to be language enabled. These are the only fields which should be returned.
+   */
+  static getLanguageTaggedProperties(source: unknown, requiredFieldNames: Array<string>): Map<string, string> {
     const languageTagEnabledFieldsNamesMapping: Map<string, string> = new Map<string, string>();
-    languageTagEnabledFieldsNames.forEach((value) => languageTagEnabledFieldsNamesMapping.set(value, value));
+    requiredFieldNames.forEach((value) => languageTagEnabledFieldsNamesMapping.set(value, value));
     return this.getLanguageTaggedPropertiesMapped(source, languageTagEnabledFieldsNamesMapping);
   }
 
-  static getLanguageTaggedPropertiesMapped(source: unknown, languageTagEnabledFieldsNamesMapping: Map<string, string>): Map<string, string> {
+  /**
+   * It will give back a fields which are language tag enabled and are mapped in the required fields.
+   *
+   * @param source is the object from which the language enabled fields and their values will be extracted.
+   * @param requiredFieldNamesMapping the fields which are supposed to be language enabled. These are the only fields which should be returned. And
+   *                                  the fields names will be transformed as per the mapping provided.
+   */
+  static getLanguageTaggedPropertiesMapped(source: unknown, requiredFieldNamesMapping: Map<string, string>): Map<string, string> {
     this.assertSourceIsWorthChecking(source);
-    this.assertValidTargetFieldNames(languageTagEnabledFieldsNamesMapping);
+    this.assertValidTargetFieldNames(requiredFieldNamesMapping);
 
     const discoveredLanguageTaggedFields: Map<string, string> = new Map<string, string>();
 
@@ -29,7 +48,7 @@ export default class LanguageTagUtils {
           key,
           value as string,
           languageTagSeparatorIndexInKey,
-          languageTagEnabledFieldsNamesMapping,
+          requiredFieldNamesMapping,
           discoveredLanguageTaggedFields
         );
       }
